cf1379cd3378bd0557446a4850ac20ebab7d75e7
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2024-10-26 17:31:33 +0300

    chore: 🔧 module refactoring

---
+++ Backend/src/test/java/ru/ifmo/se/backend/OperariiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.se.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperariiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
